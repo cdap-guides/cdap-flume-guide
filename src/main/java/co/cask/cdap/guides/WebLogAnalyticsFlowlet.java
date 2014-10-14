80b13b6b0b34debcb1cb4a0b8a2e241978ea66a3
Gowri Shankar Panneer Selvam <shankar@Shankar-MacBook-Pro.local>
2014-10-14 16:27:05 -0700

    webLogAnalyticsApp checkin

---
+++ src/main/java/co/cask/cdap/guides/WebLogAnalyticsFlowlet.java
@@ -0,0 +1,39 @@
+package co.cask.cdap.guides;
+
+import co.cask.cdap.api.annotation.ProcessInput;
+import co.cask.cdap.api.annotation.UseDataSet;
+import co.cask.cdap.api.common.Bytes;
+import co.cask.cdap.api.dataset.lib.KeyValueTable;
+import co.cask.cdap.api.flow.flowlet.AbstractFlowlet;
+import co.cask.cdap.api.flow.flowlet.StreamEvent;
+import com.google.common.base.Charsets;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Flowlet recevies weblog as stream event; parses and extracts the requested page and increments the pagecount for
+ * the webpage in the dataset
+ */
+public class WebLogAnalyticsFlowlet extends AbstractFlowlet {
+  static final Pattern ACCESS_LOG_PATTERN = Pattern.compile(
+    //   IP       id    user      date          request     code     size    referrer    user agent
+    "^([\\d.]+) (\\S+) (\\S+) \\[([^\\]]+)\\] \"([^\"]+)\" (\\d{3}) (\\d+) \"([^\"]+)\" \"([^\"]+)\"");
+
+  @UseDataSet("pageViewTable")
+  KeyValueTable pageViewTable;
+
+  @ProcessInput
+  public void process(StreamEvent log) {
+    String event = Charsets.UTF_8.decode(log.getBody()).toString();
+    Matcher matcher = ACCESS_LOG_PATTERN.matcher(event);
+    if (!matcher.matches() || matcher.groupCount() < 8) {
+      return;
+    }
+    String request = matcher.group(5);
+    int startIndex = request.indexOf(" ");
+    int endIndex = request.indexOf(" ", startIndex + 1);
+    String uri = request.substring(startIndex + 1, endIndex);
+    pageViewTable.increment(Bytes.toBytes(uri), 1L);
+  }
+}
